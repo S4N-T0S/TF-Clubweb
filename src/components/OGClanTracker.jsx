e9dd2a7a3d640cb6fcf441dc6b3963a57326c8e1
Feel It <170260504+S4N-T0S@users.noreply.github.com>
2024-12-22 18:12:19 +0000

    scruy shoutout

--- src/components/OGClanTracker.jsx
+++ src/components/OGClanTracker.jsx
@@ -82,7 +82,7 @@ const SearchBar = ({ value, onChange }) => (
       type="text"
       value={value}
       onChange={(e) => onChange(e.target.value)}
-      placeholder="Search players..."
+      placeholder="Search players or club tags! Or even both e.g: [OG] ttvscruy"
       className="w-full pl-10 pr-4 py-2 border rounded-lg bg-gray-700 border-gray-600 text-gray-200 focus:outline-none focus:border-blue-500"
     />
   </div>
