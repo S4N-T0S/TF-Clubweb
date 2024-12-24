7116899e4691f05b9ff3ca7b8ed5465e25606d39
Feel It <170260504+S4N-T0S@users.noreply.github.com>
2024-12-24 13:51:42 +0000

    missing member

--- src/data/clanMembers.js
+++ src/data/clanMembers.js
@@ -5,285 +5,241 @@ const ogClanMembers = [
     {
       discord: "tsa.gov",
       embarkId: "00#0000",
-      type: "server",
       pruby: true
     },
     {
       discord: "papichewlo.",
       embarkId: "ttvscruy#0018",
-      type: "server",
       pruby: true
     },
     {
       discord: "xorog",
       embarkId: "Xorog#3439",
-      type: "server",
       pruby: true
     },
     {   
       discord: "dr_asian",
       embarkId: "Exenity#5508",
-      type: "server",
       pruby: true
     },
     {
       discord: "sakerarenai",
       embarkId: "Sakerarenai#2742",
-      type: "server",
       pruby: true
     },
     {
       discord: "derbaumstamm",
       embarkId: "DieserBaumstamm#4372",
-      type: "server",
       pruby: true
     },
     {
       discord: "crack_whqre",
       embarkId: "Player#6491",
-      type: "server",
       pruby: true
     },
     {
       discord: "flexii.",
       embarkId: "RicRicardo#6962",
-      type: "server",
       pruby: true
     },
     {
       discord: "rykerz0",
       embarkId: "ryker#1167",
-      type: "server",
       pruby: true
     },
     {
       discord: "synphomia",
       embarkId: "Uwu#8811",
-      type: "server",
       pruby: true
     },
     {
       discord: "pizzaseverythin",
       embarkId: "Pizzaseverythin#0496",
-      type: "server",
       pruby: true
     },
     {
       discord: "allinen.",
       embarkId: "alli#1120",
-      type: "server",
       pruby: true
     },
     {
       discord: "aurash.c",
       embarkId: "aurash#4890",
-      type: "server",
       pruby: true
     },
     {
       discord: "iiendos",
       embarkId: "Endos#1663",
-      type: "server",
       pruby: true
     },
     {
       discord: "rfc6137",
       embarkId: "WittyTrap#7545",
-      type: "server",
       pruby: true
     },
     {
       discord: "ithorn",
       embarkId: "iThorn#8627",
-      type: "server",
       pruby: true
     },
     {
       discord: "tuomashan_se",
       embarkId: "Virtahevonen666#1125",
-      type: "server",
       pruby: false
     },
     {
       discord: "993.rs",
       embarkId: "AymOn#2921",
-      type: "server",
       pruby: false
     },
     {
       discord: "pol._.",
       embarkId: "Speedy#0850",
-      type: "server",
       pruby: false
     },
     {
       discord: "_dejwid_",
       embarkId: "Dejwid#1239",
-      type: "server",
       pruby: false
     },
     {
       discord: "etiquette",
       embarkId: "Etiquette#6690",
-      type: "server",
       pruby: false
     },
     {
       discord: "noobgamer2014",
       embarkId: "MangoPflücker#4332",
-      type: "server",
       pruby: false
     },
     {
       discord: "mtene",
       embarkId: "i_drive#6983",
-      type: "server",
       pruby: false
     },
     {
       discord: "gingergenius",
       embarkId: "vanvelze#8960",
-      type: "server",
       pruby: false
     },
     {
       discord: "jakubi.",
       embarkId: "Jakubi#7293",
-      type: "server",
       pruby: false
     },
     {
       discord: "rpika_",
       embarkId: "TTV-RP1KA#0506",
-      type: "server",
       pruby: false
     },
     {
       discord: "d1zzer_",
       embarkId: "Dizzer#1198",
-      type: "server",
       pruby: false
     },
     {
       discord: "live",
       embarkId: "livetolove#8992",
-      type: "server",
       pruby: false
     },
     {
       discord: "ripmirage",
       embarkId: "RIPMIRAGE#5254",
-      type: "server",
       pruby: false
     },
     {
       discord: "hebson",
       embarkId: "HebZ#7420",
-      type: "server",
       pruby: false
     },
     {
       discord: "erikponti",
       embarkId: "ErikPonti#3259",
-      type: "server",
       pruby: false
     },
     {
       discord: "funk1573",
       embarkId: "funkzer0#5832",
-      type: "server",
       pruby: false
     },
     {
       discord: "beernymph",
       embarkId: "BeerNymph#2975",
-      type: "server",
       pruby: false
     },
     {
       discord: "stolen.username",
       embarkId: "oo#8050",
-      type: "server",
       pruby: false
     },
     {
       discord: "srmanzanottv",
       embarkId: "SrManzano.TTV#6889",
-      type: "server",
       pruby: false
     },
     {
       discord: "_bysimon_",
       embarkId: "bySimon#7032",
-      type: "server",
       pruby: true
     },
     {
       discord: "genieatom",
       embarkId: "GenieAtom#0247",
-      type: "server",
       pruby: true
     },
     {
       discord: "epicmode",
       embarkId: "EpicMode#3936",
-      type: "server",
       pruby: false
     },
   
     // Non Server Members
     {
       embarkId: "MAS#0686",
-      type: "regular",
       pruby: true
     },
     {
       embarkId: "Kdohi#6229",
-      type: "regular",
       pruby: true
     },
     {
       embarkId: "Quickzz#6228",
-      type: "regular",
       pruby: true
     },
     {
       discord: "lifeless_life",
       embarkId: "Sislover#1628",
-      type: "server",
       pruby: false
     },
     {
       discord: "julzr",
       embarkId: "Julz#8685",
-      type: "server",
       pruby: false
     },
     {
       embarkId: "Neo#9269",
-      type: "regular",
       pruby: false
     },
     {
       embarkId: "dany#1324",
-      type: "regular",
       pruby: true
     },
     {
       embarkId: "Botel#2308",
-      type: "regular",
       pruby: false
     },
     {
       discord: "highcat6607",
       embarkId: "Madrif#7737",
-      type: "server",
       pruby: false
     },
     {
       discord: "punished_weatlycore47",
       embarkId: "weatlycore47#7517",
-      type: "server",
+      pruby: false
+    },
+    {
+      embarkId: "Caljim#1480",
       pruby: false
     }
   ];
