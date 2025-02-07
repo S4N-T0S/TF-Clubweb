5537f1c43037b45f0938e25a1911444ecf21798e
Diogo Santos <170260504+S4N-T0S@users.noreply.github.com>
2025-02-07 18:03:06 +0000

    fix: bugfixed confusing transform-origins which caused unfavoriting to go left to right... Cleaned up CSS a bit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,30 +7,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        'favorite-fill': {
-          '0%': {
-            background: 'linear-gradient(to right, rgba(234, 179, 8, 0.1) 0%, transparent 0%)'
-          },
-          '100%': {
-            background: 'linear-gradient(to right, rgba(234, 179, 8, 0.1) 100%, transparent 100%)'
-          }
-        },
-        'unfavorite-fill': {
-          '0%': {
-            background: 'linear-gradient(to left, transparent 0%, rgba(234, 179, 8, 0.1) 0%)'
-          },
-          '100%': {
-            background: 'linear-gradient(to left, transparent 100%, rgba(234, 179, 8, 0.1) 100%)'
-          }
-        }
-      },
-      animation: {
-        'favorite-fill': 'favorite-fill 1000ms ease-out forwards',
-        'unfavorite-fill': 'unfavorite-fill 1000ms ease-out forwards'
-      }
-    }
+    extend: {}
   },
   plugins: [
     tailwindScrollbar({ nocompatible: true }),
