df9584ae687c5279b36042dccd74fec015357a07
Diogo Santos <170260504+S4N-T0S@users.noreply.github.com>
2026-01-05 16:33:21 +0000

    Add manualChunks option to build, slight improvement to caching and compression.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {}
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Manually compress all frontend historical json data into 1 large file for better caching + compression.
+          if (id.includes('src/data/')) {
+            return 'historical-data';
+          }
+        },
+      },
+    },
+  },
 });
